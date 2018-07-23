29939c3bcedc3a000de8b22074be5c4dcb39b0ee
d.zyabko <d.zyabko@itransition.com>
2018-07-23 16:21:58 +0300

    lesson 2 mousemove

--- lesson_2/index.js
+++ lesson_2/index.js
@@ -1,11 +1,16 @@
-// var button = document.querySelector('button');
-//
-// var btn$ = rxjs.fromEvent(button, 'click');
-//
-//
-// btn$.subscribe(function(event) {
-//   console.log(event);
-// });
+var button = document.querySelector('button');
+
+var btn$ = rxjs.fromEvent(button, 'click');
+
+
+btn$.subscribe(function(event) {
+  console.log(event);
+});
 
 rxjs.fromEvent(document.querySelector('input'), 'keyup')
     .subscribe(e => console.log(e));
+
+rxjs.fromEvent(document, 'mousemove')
+  .subscribe(e => {
+    document.querySelector('h1').innerHTML = `X: ${e.clientX}, Y: ${e.clientY}`;
+  });
